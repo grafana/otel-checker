7f8768560f08388a598985d10c634edc579ce179
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-03-05 10:33:05 +0100

    java deps

--- checks/sdk/javaChecker_test.go
+++ checks/sdk/javaChecker_test.go
@@ -9,13 +9,13 @@ import (
 func TestFindSupportedLibrary(t *testing.T) {
 	modules, err := supportedLibraries()
 	require.NoError(t, err)
-	assert.Equal(t, []string{
-		"https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/instrumentation/logback/logback-mdc-1.0", "https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/instrumentation/logback/logback-appender-1.0",
-	}, findSupportedLibraries(JavaLibrary{
-		Group:    "ch.qos.logback",
-		Artifact: "logback-classic",
-		Version:  "1.5.16",
-	}, modules))
+	assert.Equal(t,
+		[]string{"https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/instrumentation/logback/logback-appender-1.0"},
+		findSupportedLibraries(JavaLibrary{
+			Group:    "ch.qos.logback",
+			Artifact: "logback-classic",
+			Version:  "1.5.16",
+		}, modules))
 }
 
 func TestParseGradleDependencies(t *testing.T) {
