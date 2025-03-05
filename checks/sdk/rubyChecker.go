86b626e030dfdec4b3ab4d3f6d9d448a18dc940e
Sean Packham <sean.packham@grafana.com>
2025-03-05 15:09:02 +0000

    Update rubyChecker.go

--- checks/sdk/rubyChecker.go
+++ checks/sdk/rubyChecker.go
@@ -12,24 +12,63 @@ import (
 func CheckRubySetup(reporter *utils.ComponentReporter, manualInstrumentation bool) {
 	checkRubyVersion(reporter)
 	checkBundlerInstalled(reporter)
-	if !manualInstrumentation {
-		checkRubyAutoInstrumentation(reporter)
-	} else {
-		checkRubyCodeBasedInstrumentation(reporter)
+
+	gemfile, err := checkGemfileExists(reporter)
+	if err != nil {
+		return
 	}
+
+	// shared instrumentation checks
+	checkRubyRequiredInstrumentation(reporter, &gemfile)
+
+	if !manualInstrumentation {
+		checkRubyAutoInstrumentation(reporter, &gemfile)
+	}
+
+	// code based instrumentation uses the required instrumentation gems
 }
 
 // While tested, support for jruby and truffleruby are on a best-effort basis at this time.
 func checkRubyVersion(reporter *utils.ComponentReporter) {
 	hasCRuby := checkCRubyVersion(reporter)
 	hasJRuby := checkJRubyVersion(reporter)
-	hasTruffleRuby := checkJRubyVersion(reporter)
+	hasTruffleRuby := checkTruffleRubyVersion(reporter)
 
 	if hasCRuby || hasJRuby || hasTruffleRuby {
 		reporter.AddSuccessfulCheck("Ruby setup successful")
 	} else {
 		reporter.AddError("No Ruby found, install CRuby >= 3.0, JRuby >= 9.3.2.0, or TruffleRuby >= 22.1")
 	}
+}
+
+func checkBundlerInstalled(reporter *utils.ComponentReporter) {
+	cmd := exec.Command("bundle", "-v")
+	_, err := cmd.Output()
+
+	if err != nil {
+		reporter.AddError("Bundler not found. Run 'gem install bundler' to install it.")
+	} else {
+		reporter.AddSuccessfulCheck("Bundler found. Run 'bundle install' to install dependencies.")
+	}
+}
+
+func checkGemfileExists(reporter *utils.ComponentReporter) (string, error) {
+	_, err := os.ReadFile("Gemfile.lock")
+	if err != nil {
+		reporter.AddError("Could not find Gemfile, create one, add dependencies, and run 'bundle install'")
+		return "", err
+	}
+
+	content, err := os.ReadFile("Gemfile.lock")
+	if err != nil {
+		reporter.AddError("Could not find Gemfile.lock run 'bundle install' to generate it")
+		return "", err
+	}
+
+	gemfile := string(content)
+	reporter.AddSuccessfulCheck("Found Gemfile.lock")
+
+	return gemfile, nil
 }
 
 func checkCRubyVersion(reporter *utils.ComponentReporter) bool {
@@ -68,75 +107,86 @@ func checkJRubyVersion(reporter *utils.ComponentReporter) bool {
 	}
 }
 
-func checkBundlerInstalled(reporter *utils.ComponentReporter) {
-	cmd := exec.Command("bundle", "-v")
-	_, err := cmd.Output()
+// not implemented yet
+func checkTruffleRubyVersion(reporter *utils.ComponentReporter) bool {
+	return false
+}
 
-	if err != nil {
-		reporter.AddError("Bundler not found. Run 'gem install bundler' to install it.")
-	} else {
-		reporter.AddSuccessfulCheck("Bundler found. Run 'bundle install' to install dependencies.")
+func checkRubyRequiredInstrumentation(reporter *utils.ComponentReporter, gemfile *string) {
+	requiredGems := []string{
+		"opentelemetry-api",
+		"opentelemetry-sdk",
+		"opentelemetry-exporter-otlp",
+	}
+
+	// loop through requiredGs and check if they are in Gemfile.lock
+	for _, gem := range requiredGems {
+		if strings.Contains(*gemfile, gem) {
+			reporter.AddSuccessfulCheck("Found required dependency: " + gem)
+		} else {
+			reporter.AddError("Missing required dependency: " + gem + ", add it to your Gemfile and run 'bundle install'")
+		}
 	}
 }
 
-// TruffleRuby check is not supported yet
-//func checkTruffleRubyVersion(reporter *utils.ComponentReporter) bool {
-//	return false
-//}
+func checkRubyAutoInstrumentation(reporter *utils.ComponentReporter, gemfile *string) {
+	allFound := false
+	optionalFound := false
 
-func checkRubyAutoInstrumentation(reporter *utils.ComponentReporter) {
-	content, err := os.ReadFile("Gemfile.lock")
-	if err != nil {
-		reporter.AddError("Could not find Gemfile.lock. Run 'bundle install' to generate it.")
-		return
+	allGem := "opentelemetry-instrumentation-all"
+
+	optionalGems := []string{
+		"opentelemetry-instrumentation-active_model_serializers",
+		"opentelemetry-instrumentation-aws_lambda",
+		"opentelemetry-instrumentation-aws_sdk",
+		"opentelemetry-instrumentation-bunny",
+		"opentelemetry-instrumentation-concurrent_ruby",
+		"opentelemetry-instrumentation-dalli",
+		"opentelemetry-instrumentation-delayed_job",
+		"opentelemetry-instrumentation-ethon",
+		"opentelemetry-instrumentation-excon",
+		"opentelemetry-instrumentation-faraday",
+		"opentelemetry-instrumentation-grape",
+		"opentelemetry-instrumentation-graphql",
+		"opentelemetry-instrumentation-gruf",
+		"opentelemetry-instrumentation-http",
+		"opentelemetry-instrumentation-http_client",
+		"opentelemetry-instrumentation-koala",
+		"opentelemetry-instrumentation-lmdb",
+		"opentelemetry-instrumentation-mongo",
+		"opentelemetry-instrumentation-mysql2",
+		"opentelemetry-instrumentation-net_http",
+		"opentelemetry-instrumentation-pg",
+		"opentelemetry-instrumentation-que",
+		"opentelemetry-instrumentation-racecar",
+		"opentelemetry-instrumentation-rack",
+		"opentelemetry-instrumentation-rails",
+		"opentelemetry-instrumentation-rake",
+		"opentelemetry-instrumentation-rdkafka",
+		"opentelemetry-instrumentation-redis",
+		"opentelemetry-instrumentation-resque",
+		"opentelemetry-instrumentation-restclient",
+		"opentelemetry-instrumentation-ruby_kafka",
+		"opentelemetry-instrumentation-sidekiq",
+		"opentelemetry-instrumentation-sinatra",
+		"opentelemetry-instrumentation-trilogy",
 	}
 
-	contentStr := string(content)
-
-	if strings.Contains(contentStr, "opentelemetry-sdk") {
-		reporter.AddSuccessfulCheck("Found required dependency: opentelemetry-sdk")
-	} else {
-		reporter.AddError("Missing required OpenTelemetry Ruby dependency: opentelemetry-sdk. Add it to your Gemfile and run 'bundle install'.")
+	if strings.Contains(*gemfile, allGem) {
+		allFound = true
+		reporter.AddSuccessfulCheck("Found optional instrumentation dependency: " + allGem)
 	}
 
-	if strings.Contains(contentStr, "opentelemetry-instrumentation-all") {
-		reporter.AddSuccessfulCheck("Found required dependency: opentelemetry-instrumentation-all")
-	} else {
-		reporter.AddError("Missing required OpenTelemetry Ruby dependency: opentelemetry-instrumentation-all. Add it to your Gemfile and run 'bundle install'.")
+	// loop through requiredGs and check if they are in Gemfile.lock
+	for _, gem := range optionalGems {
+		if strings.Contains(*gemfile, gem) {
+			optionalFound = true
+			reporter.AddSuccessfulCheck("Found optional instrumentation dependency: " + gem)
+		}
+	}
+
+	// if not allFound or not optionalFound then add error
+	if !allFound || !optionalFound {
+		reporter.AddError("Missing instrumentation dependencies, add them to your Gemfile and run 'bundle install'")
 	}
 }
-
-func checkRubyCodeBasedInstrumentation(reporter *utils.ComponentReporter) {
-	content, err := os.ReadFile("Gemfile.lock")
-	if err != nil {
-		reporter.AddError("Could not find Gemfile.lock. Run 'bundle install' to generate it.")
-		return
-	}
-
-	contentStr := string(content)
-
-	if strings.Contains(contentStr, "opentelemetry-sdk") {
-		reporter.AddSuccessfulCheck("Found required dependency: opentelemetry-sdk")
-	} else {
-		reporter.AddError("Missing required OpenTelemetry Ruby dependency: opentelemetry-sdk. Add it to your Gemfile and run 'bundle install'.")
-	}
-
-	if strings.Contains(contentStr, "opentelemetry-api") {
-		reporter.AddSuccessfulCheck("Found required dependency: opentelemetry-api")
-	} else {
-		reporter.AddError("Missing required OpenTelemetry Ruby dependency: opentelemetry-api. Add it to your Gemfile and run 'bundle install'.")
-	}
-
-	if strings.Contains(contentStr, "opentelemetry-common") {
-		reporter.AddSuccessfulCheck("Found required dependency: opentelemetry-common")
-	} else {
-		reporter.AddError("Missing required OpenTelemetry Ruby dependency: opentelemetry-common. Add it to your Gemfile and run 'bundle install'.")
-	}
-}
-
-// possibly check instrumentation file for presence of following strings:
-// OpenTelemetry.tracer_provider.tracer
-// OpenTelemetry.propagation.extract
-// OpenTelemetry::Context.with_current
-// tracer.in_span
-// span.set_attribute
