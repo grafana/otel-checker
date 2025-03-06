d54b4870c593dc92b88665fbe273c022e2e2eb2c
Matthew Hensley <matthew.hensley@grafana.com>
2025-03-05 22:04:22 -0500

    checkDotNetVersion

--- checks/sdk/dotnetChecker.go
+++ checks/sdk/dotnetChecker.go
@@ -1,6 +1,13 @@
 package sdk
 
-import "otel-checker/checks/utils"
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+	"otel-checker/checks/utils"
+)
+
+const minDotNetVersion = 8
 
 func CheckDotNetSetup(reporter *utils.ComponentReporter, commands utils.Commands) {
 	checkDotNetVersion(reporter)
@@ -11,7 +18,35 @@ func CheckDotNetSetup(reporter *utils.ComponentReporter, commands utils.Commands
 	}
 }
 
-func checkDotNetVersion(reporter *utils.ComponentReporter) {}
+func checkDotNetVersion(reporter *utils.ComponentReporter) {
+	cmd := exec.Command("dotnet", "--version")
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		reporter.AddError(fmt.Sprintf("Could not check .NET version: %s", err))
+		return
+	}
+
+	version := strings.TrimSpace(string(stdout))
+	versionParts := strings.Split(version, ".")
+	if len(versionParts) == 0 {
+		reporter.AddError("Could not parse .NET version: version string is empty")
+		return
+	}
+	majorVersion := versionParts[0]
+	v, err := strconv.Atoi(majorVersion)
+
+	if err != nil {
+		reporter.AddError(fmt.Sprintf("Could not parse .NET version: %s", err))
+		return
+	}
+
+	if v >= minDotNetVersion {
+		reporter.AddSuccessfulCheck(fmt.Sprintf("Using .NET version equal or greater than minimum recommended (%d.0)", minDotNetVersion))
+	} else {
+		reporter.AddError(fmt.Sprintf("Not using recommended .NET version. Update your .NET SDK to at least version %d.0", minDotNetVersion))
+	}
+}
 
 func checkDotNetAutoInstrumentation(reporter *utils.ComponentReporter) {}
 
